64122fe30dc89a26a28d46dc8e483dc7d68801ec
Thabiso Sekhwela <skhaib001@myuct.ac.za>
2022-06-19 01:18:02 +0200

    initial commit

--- components/About.jsx
+++ components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             / / I am not your average developer
           </p>
           <p className='py-2 text-gray-600'>
-            I'm Thabiso Sekhwela, Born and Raised in Tembisa, Gauteng. I'm a recent graduate from the University of Cape Town in Mechatronics Engineering because  it allows me to look at tech in three different angles from software to electrical and mechanical engineering. I am an individual who believes that technology can be used as an innstrument to change the world and influence peoples lives positively. I am philanthropist at heart, In my spare time I volunteer tutoring Mathematics and Physical Science in my community.
+            I&apos;m Thabiso Sekhwela, Born and Raised in Tembisa, Gauteng. I&apos;m a recent graduate from the University of Cape Town in Mechatronics Engineering because  it allows me to look at tech in three different angles from software to electrical and mechanical engineering. I am an individual who believes that technology can be used as an innstrument to change the world and influence peoples lives positively. I am philanthropist at heart, In my spare time I volunteer tutoring Mathematics and Physical Science in my community.
           </p>
           <p className='py-2 text-gray-600'>
             Fascinated with how intricate programming can be I was quickly drawn
@@ -27,7 +27,7 @@ const About = () => {
           </p>
           <h2 className='py-4'>Why I want to be a developer at SovTech</h2>
           <p>
-              Software engineering is the centre piece of the 4th industrial revolution. SovTech is a reputable company. South Africa has not reached its full potential in the 4th industrial revolution, I believe that the SovTech could help accelerate and catalyst South Africa's potential in the 4th industrial revolution. I am an innovation individual who will add value to the software industry, being a software developer at SovTech will increase the range of my development skills allows me to be of great substance to the team. As a person of Color I recognise that there's a shortage of programming skills in my community and I feel that by being given this opportunity my responsible will be to bridge this gap.
+              Software engineering is the centre piece of the 4th industrial revolution. SovTech is a reputable company. South Africa has not reached its full potential in the 4th industrial revolution, I believe that the SovTech could help accelerate and catalyst South Africa&apos;s potential in the 4th industrial revolution. I am an innovation individual who will add value to the software industry, being a software developer at SovTech will increase the range of my development skills allows me to be of great substance to the team. As a person of Color I recognise that there&apos;s a shortage of programming skills in my community and I feel that by being given this opportunity my responsible will be to bridge this gap.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
