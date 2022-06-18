82c6da5c4bf339b7ed34543ad4bfb5e83c6a93a5
Thabiso Sekhwela <skhaib001@myuct.ac.za>
2022-06-19 00:54:12 +0200

    first commit

---
+++ pages/resume.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>Thabiso | Resume</title>
+        <meta
+          name='description'
+          content='I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences.'
+        />
+        <link rel='icon' href='/fav.png' />
+      </Head>
+
+      <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
+        <h2 className='text-center'>Resume</h2>
+        <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
+          <h2 className='text-center'>Thabis Sekhwela</h2>
+          <div className='flex'>
+            <a
+              href='https://www.linkedin.com/in/clint-briley-50056920a/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
+            </a>
+            <a
+              href='https://github.com/fireclint'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaGithub size={20} style={{ marginRight: '1rem' }} />
+            </a>
+          </div>
+        </div>
+        <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
+          <p>
+            Proven Leadership <span className='px-1'>|</span> Software Development{' '}
+            <span className='px-1'>|</span> Complex Problem Solving
+          </p>
+        </div>
+        <p>
+          Analytical, innovative, and motivated web development professional
+          with experience in customer service, team leadership, and
+          organizational effectiveness in fast-paced and challenging
+          environments. Adept at developing strategies and driving streamlined
+          operations. Diverse analytical skills, team collaboration, and
+          relationship building. Consummate professional, and motivated leader,
+          with solid interpersonal abilities and complex problem-solving skills.
+          Effective and proven track record of critical thinking, idea
+          generation, and optimizing efficiencies.
+        </p>
+
+        {/* Skills */}
+        <div className='text-center py-4'>
+          <h5 className='text-center underline text-[18px] py-2'>Skills</h5>
+          <p className='py-2'>
+            <span className='font-bold'>Technical Skills</span>
+            <span className='px-2'>|</span>Front-End Web Developer
+            <span className='px-2'>|</span> HTML <span className='px-2'>|</span>
+            CSS <span className='px-2'>|</span>React
+            <span className='px-2'>|</span>Tailwind
+            <span className='px-2'>|</span> Firebase
+          </p>
+          
+        </div>
+
+        
+      </div>
+    </>
+  );
+};
+
+export default resume;
